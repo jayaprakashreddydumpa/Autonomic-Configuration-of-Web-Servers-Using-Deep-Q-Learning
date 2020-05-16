953b577b734ab3f99f46af7863b0a169a2ba3299
Jaya Prakash Reddy <47201230+jayaprakashreddydumpa@users.noreply.github.com>
2020-05-15 22:31:43 -0700

    Update dqn_test.py

--- dqn_test.py
+++ dqn_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.models import load_model
 np.warnings.filterwarnings('ignore')
 import subprocess
-import dummy
+import webserver_config_dqn
 
 def test(model, env):
     current_state = env.reset()
@@ -15,13 +15,10 @@ def test(model, env):
 
 
 if __name__ == "__main__":
-    env = dummy.Apache_environment()
+    env = webserver_config_dqn.Apache_environment()
 
-    #Getiing back the best reward from the training of the DQNAgent on the environment
-    #Loading the model based on the best_reward obtained from the saved models
-    models = ['DQNAgent__best_reward____91.67.model','DQNAgent__best_reward____36.79.model','DQNAgentfinal.model']
-    for model in models:
-        LOAD_MODEL = "models/"+str(model)
-        test_model = load_model(LOAD_MODEL)
-        reward = test(test_model,env)
-        print("The reward obtained is: ",reward)
+    #Loading the model based on the best_reward obtained from the saved model
+    LOAD_MODEL = "models/DQNAgentfinal.model"
+    test_model = load_model(LOAD_MODEL)
+    reward = test(test_model,env)
+    print("The reward obtained is: ",reward)
